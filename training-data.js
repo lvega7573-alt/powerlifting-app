8c66a1fd116f406678c7df655ab307e0790f7d20
lvega7573-alt <lvega7573@gmail.com>
2026-03-06 18:32:36 -0300

    Update and rename training.js to training-data.js

---
+++ training-data.js
@@ -0,0 +1,103 @@
+
+const trainingDays=[
+
+{
+name:"Día 1",
+exercises:[
+{name:"Bench Press",sets:5,weight:95},
+{name:"Squat",sets:5,weight:120}
+]
+},
+
+{
+name:"Día 2",
+exercises:[
+{name:"Deadlift",sets:5,weight:130}
+]
+},
+
+{
+name:"Día 3",
+exercises:[
+{name:"Bench Press",sets:4,weight:100}
+]
+},
+
+{
+name:"Día 4",
+exercises:[
+{name:"Squat",sets:4,weight:130}
+]
+},
+
+{
+name:"Día 5",
+exercises:[
+{name:"Deadlift",sets:4,weight:140}
+]
+},
+
+{
+name:"Día 6",
+exercises:[
+{name:"Bench Press",sets:3,weight:110}
+]
+},
+
+{
+name:"Día 7",
+exercises:[
+{name:"Squat",sets:3,weight:140}
+]
+},
+
+{
+name:"Día 8",
+exercises:[
+{name:"Bench Press",sets:3,weight:115}
+]
+},
+
+{
+name:"Día 9",
+exercises:[
+{name:"Deadlift",sets:3,weight:150}
+]
+},
+
+{
+name:"Día 10",
+exercises:[
+{name:"Squat",sets:3,weight:150}
+]
+},
+
+{
+name:"Día 11",
+exercises:[
+{name:"Bench Press",sets:2,weight:120}
+]
+},
+
+{
+name:"Día 12",
+exercises:[
+{name:"Deadlift",sets:2,weight:160}
+]
+},
+
+{
+name:"Día 13",
+exercises:[
+{name:"Squat",sets:2,weight:155}
+]
+},
+
+{
+name:"Día 14",
+exercises:[
+{name:"Test Max",sets:1,weight:"MAX"}
+]
+}
+
+]
