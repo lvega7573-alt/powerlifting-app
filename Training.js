eca6b7a697250f693a3c3199871afe208895163b
lvega7573-alt <lvega7573@gmail.com>
2026-03-06 16:01:20 -0300

    Update Training.js

--- Training.js
+++ Training.js
@@ -4,37 +4,35 @@ const TRAINING_DAYS = {
 title:"Día 1 – Sentadilla pesada",
 blocks:[
 
-{type:"text",label:"Entrada en calor movilidad",
+{type:"text",label:"Entrada en calor movilidad"},
 
-{type:"exercise",name:"Movilidad Cadera",
-sets:[
-{kg:Body,reps:15,rest:60},
-{kg:Body,reps:15,rest:60},
+{type:"exercise",name:"Movilidad Cadera",sets:[
+{kg:"Body",reps:15,rest:60},
+{kg:"Body",reps:15,rest:60}
+]},
 
-{type:"exercise",name:"Movilidad Tobillos",
-sets:[
-{kg:Body,reps:15,rest:60},
-{kg:Body,reps:15,rest:60},
+{type:"exercise",name:"Movilidad Tobillos",sets:[
+{kg:"Body",reps:15,rest:60},
+{kg:"Body",reps:15,rest:60}
+]},
 
-{type:"exercise",name:"Puente de gluteos",
-sets:[
+{type:"exercise",name:"Puente de gluteos",sets:[
 {kg:60,reps:15,rest:60},
-{kg:60,reps:15,rest:60},
+{kg:60,reps:15,rest:60}
+]},
 
-{type:"text",label:"Activación",
+{type:"text",label:"Activación"},
 
-{type:"exercise",name:"Sentadillas",
-sets:[
+{type:"exercise",name:"Sentadillas",sets:[
 {kg:20,reps:10,rest:60},
 {kg:20,reps:10,rest:60},
 {kg:40,reps:10,rest:60},
 {kg:60,reps:5,rest:60},
 {kg:80,reps:3,rest:60},
-{kg:95,reps:2,rest:180},
+{kg:95,reps:2,rest:180}
+]},
 
-
-{type:"exercise",name:"Back Squat",
-sets:[
+{type:"exercise",name:"Back Squat",sets:[
 {kg:105,reps:5,rest:180},
 {kg:105,reps:5,rest:180},
 {kg:105,reps:5,rest:180},
@@ -42,77 +40,67 @@ sets:[
 {kg:105,reps:5,rest:180}
 ]},
 
-{type:"exercise",name:"Back Squat pesado",
-sets:[
+{type:"exercise",name:"Back Squat pesado",sets:[
 {kg:120,reps:3,rest:240},
 {kg:120,reps:3,rest:240},
 {kg:120,reps:3,rest:240},
 {kg:120,reps:3,rest:240}
 ]},
 
-{type:"exercise",name:"Back Squat Doble pesada",
-sets:[
+{type:"exercise",name:"Back Squat Doble pesada",sets:[
 {kg:132,reps:2,rest:240},
 {kg:132,reps:2,rest:240},
 {kg:132,reps:2,rest:240}
+]},
+
+{type:"exercise",name:"Bulgaras con Mancuernas",sets:[
+{kg:30,reps:8,rest:90},
+{kg:30,reps:8,rest:90},
+{kg:30,reps:8,rest:90}
+]},
+
+{type:"exercise",name:"Curl femorales en maquina",sets:[
+{kg:60,reps:8,rest:60},
+{kg:60,reps:8,rest:60},
+{kg:60,reps:8,rest:60}
+]},
+
+{type:"exercise",name:"Plancha",sets:[
+{kg:"Body",reps:"30 seg",rest:60},
+{kg:"Body",reps:"30 seg",rest:60},
+{kg:"Body",reps:"30 seg",rest:60},
+{kg:"Body",reps:"30 seg",rest:60}
 ]}
 
-{type:"exercise",name:"Bulgaras con Mancuernas",
-sets:[
-{kg:30,reps:8,rest:90},
-{kg:30,reps:8,rest:90},
-{kg:30,reps:8,rest:90},
 ]},
-
-{type:"exercise",name:"Curl femorales en maquina",
-sets:[
-{kg:60,reps:8,rest:60},
-{kg:60,reps:8,rest:60},
-{kg:60,reps:8,rest:60},
-]},
-
-{type:"exercise",name:"Curl femorales en maquina",
-sets:[
-{kg:Body,reps:30 seg,rest:60},
-{kg:Body,reps:30 seg,rest:60},
-{kg:Body,reps:30 seg,rest:60},
-{kg:Body,reps:30 seg,rest:60},
-]},
-
-]
-},
 
 2:{
 title:"Día 2 – Banca volumen",
 blocks:[
 
-{type:"text",label:"Entrada en calor movilidad",
+{type:"text",label:"Entrada en calor movilidad"},
 
-{type:"exercise",name:"Movilidad hombros rotadores banda",
-sets:[
-{kg:Banda,reps:15,rest:60},
-{kg:Banda,reps:15,rest:60},
-{kg:Banda,reps:15,rest:60},
+{type:"exercise",name:"Movilidad hombros banda",sets:[
+{kg:"Banda",reps:15,rest:60},
+{kg:"Banda",reps:15,rest:60},
+{kg:"Banda",reps:15,rest:60}
+]},
 
-
-{type:"exercise",name:"Face Pull",
-sets:[
+{type:"exercise",name:"Face Pull",sets:[
 {kg:30,reps:15,rest:60},
-{kg:30,reps:15,rest:60},
+{kg:30,reps:15,rest:60}
+]},
 
+{type:"text",label:"Activación"},
 
-{type:"text",label:"Activación",
-
-{type:"exercise",name:"Bench Press",
-sets:[
+{type:"exercise",name:"Bench Press",sets:[
 {kg:20,reps:10,rest:60},
 {kg:60,reps:10,rest:60},
 {kg:70,reps:10,rest:60},
-{kg:80,reps:5,rest:180},
+{kg:80,reps:5,rest:180}
+]},
 
-
-{type:"exercise",name:"Bench Press",
-sets:[
+{type:"exercise",name:"Bench Press volumen",sets:[
 {kg:90,reps:4,rest:180},
 {kg:90,reps:4,rest:180},
 {kg:90,reps:4,rest:180},
@@ -121,61 +109,54 @@ sets:[
 {kg:90,reps:4,rest:180}
 ]},
 
-{type:"exercise",name:"Bench pausa",
-sets:[
+{type:"exercise",name:"Bench pausa",sets:[
 {kg:97,reps:3,rest:180},
 {kg:97,reps:3,rest:180},
 {kg:97,reps:3,rest:180},
 {kg:97,reps:3,rest:180}
+]},
+
+{type:"exercise",name:"Inclinado con Mancuernas",sets:[
+{kg:30,reps:8,rest:90},
+{kg:30,reps:8,rest:90},
+{kg:30,reps:8,rest:90},
+{kg:30,reps:8,rest:90}
+]},
+
+{type:"exercise",name:"Fondos",sets:[
+{kg:"Body+10",reps:6,rest:90},
+{kg:"Body+10",reps:6,rest:90},
+{kg:"Body+10",reps:6,rest:90},
+{kg:"Body+10",reps:6,rest:90}
 ]}
-{type:"exercise",name:"Inclinado con Mancuernas",
-sets:[
-{kg:30,reps:8,rest:90},
-{kg:30,reps:8,rest:90},
-{kg:30,reps:8,rest:90},
-{kg:30,reps:8,rest:90},
-]},
 
-{type:"exercise",name:"Fondo",
-sets:[
-{kg:Body+10Kg,reps:6,rest:90},
-{kg:Body+10Kg,reps:6,rest:90},
-{kg:Body+10Kg,reps:6,rest:90},
-{kg:Body+10Kg,reps:6,rest:90},
 ]},
-
-]
-},
 
 3:{
 title:"Día 3 – Peso muerto",
 blocks:[
 
-{type:"text",label:"Entrada en calor movilidad",
+{type:"text",label:"Entrada en calor movilidad"},
 
-{type:"exercise",name:"Movilidad de caderas",
-sets:[
-{kg:Banda,reps:15,rest:60},
-{kg:Banda,reps:15,rest:60},
+{type:"exercise",name:"Movilidad caderas",sets:[
+{kg:"Banda",reps:15,rest:60},
+{kg:"Banda",reps:15,rest:60}
+]},
 
-
-{type:"exercise",name:"Good mornings
-sets:[
+{type:"exercise",name:"Good Morning",sets:[
 {kg:20,reps:12,rest:60},
-{kg:20,reps:12,rest:60},
+{kg:20,reps:12,rest:60}
+]},
 
+{type:"text",label:"Activación"},
 
-{type:"text",label:"Activación",
-
-{type:"exercise",name:"Deadlift",
-sets:[
+{type:"exercise",name:"Deadlift",sets:[
 {kg:60,reps:5,rest:90},
-{kg:100, reps:3, rest:90},
-{kg:115, reps:3, rest:90},
+{kg:100,reps:3,rest:90},
+{kg:115,reps:3,rest:90}
+]},
 
-
-{type:"exercise",name:"Deadlift",
-sets:[
+{type:"exercise",name:"Deadlift volumen",sets:[
 {kg:120,reps:4,rest:180},
 {kg:120,reps:4,rest:180},
 {kg:120,reps:4,rest:180},
@@ -183,187 +164,34 @@ sets:[
 {kg:120,reps:4,rest:180}
 ]},
 
-{type:"exercise",name:"Deadlift pausa rodilla",
-sets:[
+{type:"exercise",name:"Deadlift pausa rodilla",sets:[
 {kg:110,reps:3,rest:180},
 {kg:110,reps:3,rest:180},
 {kg:110,reps:3,rest:180},
 {kg:110,reps:3,rest:180}
+]},
+
+{type:"exercise",name:"Remo con barra",sets:[
+{kg:70,reps:8,rest:90},
+{kg:70,reps:8,rest:90},
+{kg:70,reps:8,rest:90},
+{kg:70,reps:8,rest:90}
+]},
+
+{type:"exercise",name:"Hip Thrust",sets:[
+{kg:120,reps:10,rest:90},
+{kg:120,reps:10,rest:90},
+{kg:120,reps:10,rest:90},
+{kg:120,reps:10,rest:90}
 ]}
 
-{type:"exercise",name:"Remo con barra",
-sets:[
-{kg:70,reps:8,rest:90},
-{kg:70,reps:8,rest:90},
-{kg:70,reps:8,rest:90},
-{kg:70,reps:8,rest:90},
 ]},
 
-{type:"exercise",name:"Hip thrust",
-sets:[
-{kg:120,reps:10,rest:90},
-{kg:120,reps:10,rest:90},
-{kg:120,reps:10,rest:90},
-{kg:120,reps:10,rest:90},
-]},
-
-]
-},
-
-4:{
-title:"Día 4 – Sentadilla velocidad",
+7:{
+title:"Día 7 – Descanso",
 blocks:[
-
-{type:"text",label:"Entrada en calor movilidad",
-
-{type:"exercise",name:"Movilidad Cadera",
-sets:[
-{kg:Body,reps:15,rest:60},
-{kg:Body,reps:15,rest:60},
-
-{type:"exercise",name:"Movilidad Tobillos",
-sets:[
-{kg:Body,reps:15,rest:60},
-{kg:Body,reps:15,rest:60},
-
-{type:"exercise",name:"Puente de gluteos",
-sets:[
-{kg:60,reps:15,rest:60},
-{kg:60,reps:15,rest:60},
-
-{type:"text",label:"Activación",
-
-{type:"exercise",name:"Sentadillas",
-sets:[
-{kg:20,reps:10,rest:60},
-{kg:20,reps:10,rest:60},
-{kg:40,reps:10,rest:60},
-{kg:60,reps:5,rest:60},
-
-{type:"exercise",name:"Speed Squat",
-sets:[
-{kg:95,reps:2,rest:90},
-{kg:95,reps:2,rest:90},
-{kg:95,reps:2,rest:90},
-{kg:95,reps:2,rest:90},
-{kg:95,reps:2,rest:90},
-{kg:95,reps:2,rest:90},
-{kg:95,reps:2,rest:90},
-{kg:95,reps:2,rest:90}
-]}
-
-{type:"exercise",name:"Speed Squat",
-sets:[
-{kg:100,reps:4,rest:180},
-{kg:100,reps:4,rest:180},
-{kg:100,reps:4,rest:180},
-{kg:100,reps:4,rest:180},
-
+{type:"text",label:"Recuperación / movilidad / caminata"}
 ]
-},
+}
 
-5:{
-title:"Día 5 – Banca pesada",
-blocks:[
-
-{type:"text",label:"Entrada en calor movilidad",
-
-{type:"exercise",name:"Movilidad hombros rotadores banda",
-sets:[
-{kg:Banda,reps:15,rest:60},
-{kg:Banda,reps:15,rest:60},
-{kg:Banda,reps:15,rest:60},
-
-
-{type:"exercise",name:"Face Pull",
-sets:[
-{kg:30,reps:15,rest:60},
-{kg:30,reps:15,rest:60},
-
-
-{type:"text",label:"Activación",
-
-{type:"exercise",name:"Bench Press",
-sets:[
-{kg:20,reps:10,rest:60},
-{kg:60,reps:10,rest:60},
-{kg:80,reps:8,rest:60},
-{kg:90,reps:5,rest:180},
-
-{type:"exercise",name:"Bench press",
-sets:[
-{kg:105,reps:3,rest:240},
-{kg:105,reps:3,rest:240},
-{kg:105,reps:3,rest:240},
-{kg:105,reps:3,rest:240},
-{kg:105,reps:3,rest:240}
-]},
-
-{type:"exercise",name:"Bench pesado",
-sets:[
-{kg:120,reps:2,rest:240},
-{kg:120,reps:2,rest:240},
-{kg:120,reps:2,rest:240}
-]},
-
-{type:"exercise",name:"Board press",
-sets:[
-{kg:115,reps:3,rest:180},
-{kg:115,reps:3,rest:180},
-{kg:115,reps:3,rest:180},
-{kg:115,reps:3,rest:180}
-]
-},
-
-6:{
-title:"Día 6 – Deadlift pesado",
-blocks:[
-
-{type:"text",label:"Entrada en calor movilidad",
-
-{type:"exercise",name:"Movilidad de caderas",
-sets:[
-{kg:Banda,reps:15,rest:60},
-{kg:Banda,reps:15,rest:60},
-
-
-{type:"exercise",name:"Good mornings
-sets:[
-{kg:20,reps:12,rest:60},
-{kg:20,reps:12,rest:60},
-
-
-{type:"text",label:"Activación",
-
-{type:"exercise",name:"Deadlift",
-sets:[
-{kg:60,reps:5,rest:90},
-{kg:100, reps:3, rest:90},
-{kg:115, reps:3, rest:90},
-
-
-{type:"exercise",name:"Deadlift",
-sets:[
-{kg:128,reps:3,rest:180},
-{kg:128,reps:3,rest:180},
-{kg:128,reps:3,rest:180},
-{kg:128,reps:3,rest:180}
-]},
-
-{type:"exercise",name:"Deadlift pesado",
-sets:[
-{kg:140,reps:2,rest:240},
-{kg:140,reps:2,rest:240},
-{kg:140,reps:2,rest:240}
-]},
-
-{type:"exercise",name:"Rack Pull",
-sets:[
-{kg:150,reps:3,rest:180},
-{kg:150,reps:3,rest:180},
-{kg:150,reps:3,rest:180}
-
-]
-},
-
-7:{title:"Día 7 – Descanso",blocks:[{type:"text",label:"Recuperación"}]},
+};
