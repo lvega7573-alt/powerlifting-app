a6dbd376cbafa5aaaf2c4849bb6c5aa2fca2ea3b
lvega7573-alt <lvega7573@gmail.com>
2026-03-06 14:01:18 -0300

    Create Training.js

---
+++ Training.js
@@ -0,0 +1,112 @@
+const training = {
+
+1:{
+title:"Sentadilla pesada",
+exercises:[
+
+{type:"warmup",text:"Movilidad y activación 15 min"},
+
+{type:"exercise",name:"Back Squat",
+sets:[
+{kg:105,reps:5,rest:180},
+{kg:105,reps:5,rest:180},
+{kg:105,reps:5,rest:180},
+{kg:105,reps:5,rest:180},
+{kg:105,reps:5,rest:180}
+]},
+
+{type:"exercise",name:"Back Squat pesado",
+sets:[
+{kg:120,reps:3,rest:240},
+{kg:120,reps:3,rest:240},
+{kg:120,reps:3,rest:240},
+{kg:120,reps:3,rest:240}
+]},
+
+{type:"exercise",name:"Doble pesada",
+sets:[
+{kg:132,reps:2,rest:240},
+{kg:132,reps:2,rest:240},
+{kg:132,reps:2,rest:240}
+]}
+
+]
+},
+
+2:{
+title:"Banca volumen",
+exercises:[
+
+{type:"exercise",name:"Bench press",
+sets:[
+{kg:90,reps:4,rest:180},
+{kg:90,reps:4,rest:180},
+{kg:90,reps:4,rest:180},
+{kg:90,reps:4,rest:180},
+{kg:90,reps:4,rest:180},
+{kg:90,reps:4,rest:180}
+]},
+
+{type:"exercise",name:"Bench pausa",
+sets:[
+{kg:97,reps:3,rest:180},
+{kg:97,reps:3,rest:180},
+{kg:97,reps:3,rest:180},
+{kg:97,reps:3,rest:180}
+]}
+
+]
+},
+
+3:{
+title:"Peso muerto",
+exercises:[
+
+{type:"exercise",name:"Deadlift",
+sets:[
+{kg:120,reps:4,rest:180},
+{kg:120,reps:4,rest:180},
+{kg:120,reps:4,rest:180},
+{kg:120,reps:4,rest:180},
+{kg:120,reps:4,rest:180}
+]},
+
+{type:"exercise",name:"Deadlift pausa",
+sets:[
+{kg:110,reps:3,rest:180},
+{kg:110,reps:3,rest:180},
+{kg:110,reps:3,rest:180},
+{kg:110,reps:3,rest:180}
+]}
+
+]
+},
+
+4:{
+title:"Sentadilla velocidad",
+exercises:[
+
+{type:"exercise",name:"Speed squat",
+sets:[
+{kg:95,reps:2,rest:90},
+{kg:95,reps:2,rest:90},
+{kg:95,reps:2,rest:90},
+{kg:95,reps:2,rest:90},
+{kg:95,reps:2,rest:90},
+{kg:95,reps:2,rest:90},
+{kg:95,reps:2,rest:90},
+{kg:95,reps:2,rest:90}
+]},
+
+{type:"exercise",name:"Front squat",
+sets:[
+{kg:100,reps:4,rest:180},
+{kg:100,reps:4,rest:180},
+{kg:100,reps:4,rest:180},
+{kg:100,reps:4,rest:180}
+]}
+
+]
+}
+
+}
