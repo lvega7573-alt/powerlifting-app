a0c8c4ff46d5f21ee90ec0b13ba70c3dcf69d254
lvega7573-alt <lvega7573@gmail.com>
2026-03-06 16:37:23 -0300

    Update Training.js

--- Training.js
+++ Training.js
@@ -1,87 +1,64 @@
 const TRAINING_DAYS = {
 
 1:{
-title:"Día 1 – Sentadilla pesada",
+title:"Día 1 - Sentadilla pesada",
 
 blocks:[
 
-{type:"text",label:"Entrada en calor"},
-
-{type:"exercise",
+{
+type:"exercise",
 name:"Back Squat",
 sets:[
-
 {kg:105,reps:5,rest:180},
 {kg:105,reps:5,rest:180},
 {kg:105,reps:5,rest:180},
 {kg:105,reps:5,rest:180},
 {kg:105,reps:5,rest:180}
+]
+},
 
-]},
-
-{type:"exercise",
+{
+type:"exercise",
 name:"Back Squat pesado",
 sets:[
-
+{kg:120,reps:3,rest:240},
 {kg:120,reps:3,rest:240},
 {kg:120,reps:3,rest:240},
 {kg:120,reps:3,rest:240}
-
-]}
+]
+}
 
 ]
 
 },
 
 2:{
-title:"Día 2 – Banca volumen",
+title:"Día 2 - Banca volumen",
 
 blocks:[
 
-{type:"exercise",
+{
+type:"exercise",
 name:"Bench Press",
 sets:[
-
+{kg:90,reps:4,rest:180},
+{kg:90,reps:4,rest:180},
+{kg:90,reps:4,rest:180},
 {kg:90,reps:4,rest:180},
 {kg:90,reps:4,rest:180},
 {kg:90,reps:4,rest:180}
-
-]}
-
 ]
-
-},
-
-3:{
-title:"Día 3 – Peso muerto",
-
-blocks:[
-
-{type:"exercise",
-name:"Deadlift",
-sets:[
-
-{kg:120,reps:4,rest:180},
-{kg:120,reps:4,rest:180},
-{kg:120,reps:4,rest:180}
-
-]}
-
-]
-
-},
-
-4:{title:"Día 4 – Sentadilla velocidad",blocks:[]},
-5:{title:"Día 5 – Banca pesada",blocks:[]},
-6:{title:"Día 6 – Deadlift pesado",blocks:[]},
-7:{title:"Día 7 – Descanso",blocks:[]},
-
-8:{title:"Día 8",blocks:[]},
-9:{title:"Día 9",blocks:[]},
-10:{title:"Día 10",blocks:[]},
-11:{title:"Día 11",blocks:[]},
-12:{title:"Día 12",blocks:[]},
-13:{title:"Día 13",blocks:[]},
-14:{title:"Día 14 Competencia",blocks:[]}
-
 }
+
+]
+
+},
+
+7:{
+title:"Día 7 - Descanso",
+blocks:[
+{type:"text",label:"Descanso total"}
+]
+}
+
+};
